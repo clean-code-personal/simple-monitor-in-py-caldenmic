be465d98fe70243ffbe8dd0dc4ce21be15a1f37a
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-15 22:58:27 +0530

    Update check_limits_production.py

--- check_limits_production.py
+++ check_limits_production.py
@@ -1,8 +1,33 @@
+name_mappings = {
+    'temperature': 'Temperature',
+    'soc': 'State of Charge', 
+    'charge_rate': 'Charge rate'
+}
+
 def battery_is_ok(temperature, soc, charge_rate):
-    attribute_ranges = {'temperature': (0, 45), 'soc': (20, 80), 'charge_rate': (0, 0.8)}
+    attribute_ranges = {
+        'temperature': (0, 45), 
+        'soc': (20, 80), 
+        'charge_rate': (0, 0.8)
+    }
+    attribute_status = {
+        'temperature': 'normal', 
+        'soc': 'normal', 
+        'charge_rate': 'normal'
+    }
     
     for attribute, (lower_limit, upper_limit) in attribute_ranges.items():
-        if not (lower_limit <= eval(attribute) <= upper_limit):
-            print(f'{attribute} is out of range!')
-            return False
-        return True
+        if (eval(attribute) < lower_limit):
+            attribute_status[attribute] = 'low'
+            # print(f'{attribute} is low!')
+        elif (eval(attribute) > upper_limit):
+            attribute_status[attribute] = 'high'
+            # print(f'{attribute} is high!')
+    battery_info(attribute_status)
+    return attribute_status
+    
+def battery_info(attribute_status):
+    print("-----------------------------------")
+    for attribute, status in attribute_status.items():
+        print(f'{name_mappings[attribute]} is {status}!')
+    print("-----------------------------------")
