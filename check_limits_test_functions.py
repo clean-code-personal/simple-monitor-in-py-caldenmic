96d3af3988d8649dab6136b8bcabf7bb831af4b9
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-15 22:59:22 +0530

    Update check_limits_test_functions.py

--- check_limits_test_functions.py
+++ check_limits_test_functions.py
@@ -3,11 +3,31 @@ from check_limits_production import battery_is_ok
 
 class TestBattery(unittest.TestCase):
     
-    def test_battery_is_ok_True(self, temperature, soc, charge_rate):
-        self.assertTrue(battery_is_ok(temperature, soc, charge_rate))
+    def test_battery_is_ok_status_all_attributes_normal(self, temperature, soc, charge_rate):
+        attribute_status = battery_is_ok(temperature, soc, charge_rate)
+        for attribute, status in attribute_status.items():
+            self.assertEqual(status, 'normal')
     
-    def test_battery_is_ok_False(self, temperature, soc, charge_rate):
-        self.assertFalse(battery_is_ok(temperature, soc, charge_rate))
+    def test_battery_is_ok_temperature_status_high(self, temperature, soc, charge_rate):
+        attribute_status = battery_is_ok(temperature, soc, charge_rate)
+        self.assertEqual(attribute_status['temperature'], 'high')
         
-    def test_charge_rate_between_0_and_1(self, charge_rate):
-        self.assertTrue(charge_rate >= 0 and charge_rate <= 1)
+    def test_battery_is_ok_temperature_status_low(self, temperature, soc, charge_rate):
+        attribute_status = battery_is_ok(temperature, soc, charge_rate)
+        self.assertEqual(attribute_status['temperature'], 'low')
+        
+    def test_battery_is_ok_soc_status_high(self, temperature, soc, charge_rate):
+        attribute_status = battery_is_ok(temperature, soc, charge_rate)
+        self.assertEqual(attribute_status['soc'], 'high')
+        
+    def test_battery_is_ok_soc_status_low(self, temperature, soc, charge_rate):
+        attribute_status = battery_is_ok(temperature, soc, charge_rate)
+        self.assertEqual(attribute_status['soc'], 'low')
+        
+    def test_battery_is_ok_charge_rate_status_high(self, temperature, soc, charge_rate):
+        attribute_status = battery_is_ok(temperature, soc, charge_rate)
+        self.assertEqual(attribute_status['charge_rate'], 'high')
+        
+    def test_battery_is_ok_charge_rate_status_low(self, temperature, soc, charge_rate):
+        attribute_status = battery_is_ok(temperature, soc, charge_rate)
+        self.assertEqual(attribute_status['charge_rate'], 'low')
