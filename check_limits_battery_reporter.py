3ecfe2a4ee0c09611c23b63cc577a1ffe99b8c06
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-16 01:15:24 +0530

    Update check_limits_battery_reporter.py

--- check_limits_battery_reporter.py
+++ check_limits_battery_reporter.py
@@ -5,8 +5,8 @@ def battery_reporter(attribute_status, abnormality):
         print("-------------------------------------------")
         print("Battery is working under optimal conditions")
         print("-------------------------------------------")
-    else:
-        print("-------------------------------------------")
-        for attribute, status in attribute_status.items():
-            print(f'{name_mappings[attribute]} is {status}!')
-        print("-------------------------------------------")
+        return
+    print("-------------------------------------------")
+    for attribute, status in attribute_status.items():
+        print(f'{name_mappings[attribute]} is {status}!')
+    print("-------------------------------------------")
