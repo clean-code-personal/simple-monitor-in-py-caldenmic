0384b095a6837849dbcca4760f9fed73beb83cea
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-16 01:11:59 +0530

    Update check_limits_production.py

--- check_limits_production.py
+++ check_limits_production.py
@@ -5,16 +5,8 @@ name_mappings = {
 }
 
 def battery_is_ok(temperature, soc, charge_rate, reporter=None):
-    attribute_ranges = {
-        'temperature': (0, 45), 
-        'soc': (20, 80), 
-        'charge_rate': (0, 0.8)
-    }
-    attribute_status = {
-        'temperature': 'normal', 
-        'soc': 'normal', 
-        'charge_rate': 'normal'
-    }
+    attribute_ranges = {'temperature': (0, 45), 'soc': (20, 80), 'charge_rate': (0, 0.8)}
+    attribute_status = {'temperature': 'normal', 'soc': 'normal', 'charge_rate': 'normal'}
     
     abnormality = 0
     
@@ -22,10 +14,8 @@ def battery_is_ok(temperature, soc, charge_rate, reporter=None):
         if (eval(attribute) < lower_limit):
             attribute_status[attribute] = 'low'
             abnormality = 1
-            # print(f'{attribute} is low!')
         elif (eval(attribute) > upper_limit):
             attribute_status[attribute] = 'high'
             abnormality = 1
-            # print(f'{attribute} is high!')
     reporter(attribute_status, abnormality)
     return attribute_status
