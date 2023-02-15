e164ac87ee4d772e5efe9d8c40fee67bc8ef5757
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-16 02:47:41 +0530

    Update check_limits_production.py

--- check_limits_production.py
+++ check_limits_production.py
@@ -1,12 +1,4 @@
 name_mappings = {'temperature': 'Temperature','soc': 'State of Charge', 'charge_rate': 'Charge rate'}
-
-def update_status(attribute, lower_limit, upper_limit, abnormality, status):
-    if (eval(attribute) < lower_limit):
-        status[attribute] = 'low'
-        abnormality = 1
-    elif (eval(attribute) > upper_limit):
-        status[attribute] = 'high'
-        abnormality = 1
         
 def battery_is_ok(temperature, soc, charge_rate, reporter=None):
     attribute_ranges = {'temperature': (0, 45), 'soc': (20, 80), 'charge_rate': (0, 0.8)}
@@ -20,3 +12,11 @@ def battery_is_ok(temperature, soc, charge_rate, reporter=None):
     print(abnormality)
     reporter(attribute_status, abnormality)
     return attribute_status
+
+def update_status(attribute, lower_limit, upper_limit, abnormality, status):
+    if (eval(attribute) < lower_limit):
+        status[attribute] = 'low'
+        abnormality = 1
+    elif (eval(attribute) > upper_limit):
+        status[attribute] = 'high'
+        abnormality = 1
