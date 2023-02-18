b7bbb30e5af73335787cce84a5838b851473d2c9
Calden Michael D'Souza <54746597+caldenmic@users.noreply.github.com>
2023-02-18 11:59:27 +0530

    Update check_limits_production.py

--- check_limits_production.py
+++ check_limits_production.py
@@ -1,59 +1,49 @@
-class Reporter:
-    def __init__(self, attribute_status):
-        self.attribute_status = attribute_status
-        self.name_mappings = {'temperature': 'Temperature', 'soc': 'State of Charge', 'charge_rate': 'Charge Rate'}
+import unittest
+from check_limits_production import Battery
 
-    def abnormality_report(self):
-        if not any(self.attribute_status):
-            self.print_report_in_english()
-            return
-        for attribute, abnormality_type in self.attribute_status.items():
-            self.print_report_in_english(attribute, abnormality_type)
+class TestBattery(unittest.TestCase):
 
-    def print_report_in_english(self, attribute = None, abnormality_type = None):
-        if not attribute and not abnormality_type:
-            print("Battery working under optimal conditions!")
-        else:
-            print(f'{self.name_mappings[attribute]} is {abnormality_type}')
+    def assertHigh(self, battery, attribute):
+        status = battery.get_attribute_satatus()
+        if attribute in status.keys():
+            self.assertEqual(status[attribute], 'high')
 
-class Battery:
-    def __init__(self, temperature, soc, charge_rate):
-        self.temperature = temperature
-        self.soc = soc
-        self.charge_rate = charge_rate
-        self.attribute_ranges = {'temperature': (0, 45), 'soc': (20, 80), 'charge_rate': (0, 0.8)}
-        self.attribute_status = {}
+    def assertLow(self, battery, attribute):
+        status = battery.get_attribute_satatus()
+        if attribute in status.keys():
+            self.assertEqual(status[attribute], 'low')
 
-    def update_attribute_status(self):
-        for attribute, (lower_limit, upper_limit) in self.attribute_ranges.items():
-            self.update_single_attribute_status(attribute, upper_limit, lower_limit)
+    def test_battery_is_okay(self):
+        battery = Battery(40, 30, 0.7)
+        battery.display_abnormality_report()
+        self.assertTrue(battery.is_battery_okay())
 
-    def update_single_attribute_status(self, attribute, upper_limit, lower_limit):
-        if (eval('self.' + attribute) < lower_limit):
-            self.attribute_status[attribute] = 'low'
-        elif (eval('self.' +attribute) > upper_limit):
-            self.attribute_status[attribute] = 'high'
-    
-    def is_battery_okay(self):
-        self.update_attribute_status()
+    def test_battery_is_not_okay(self):
+        param_list = [(50, 30, 0.7), (30, 90, 0.5), (30, 50, 0.95), (50, 90, 0.7), (50, 30, 0.90), (40, 90, 0.98), (50, 90, 0.94)]
         
-        # self.display_abnormality_report()
-        if not any(self.attribute_status):
-            return True
-        return False
+        for (temperature, soc, charge_rate) in param_list:
+            battery = Battery(temperature, soc, charge_rate)
+            battery.display_abnormality_report()
+            self.assertFalse(battery.is_battery_okay())
 
-    def number_of_abnormalities(self):
-        self.update_attribute_status()
-        return len(self.attribute_status)
+    def test_to_check_number_of_abnormalitites(self):
+        param_list_abnormality_number = [(50, 30, 0.7, 1), (30, 90, 0.5, 1), (30, 50, 0.95, 1), (50, 90, 0.7, 2), 
+        (50, 30, 0.90, 2), (40, 90, 0.98, 2), (50, 90, 0.94, 3)]
 
-    def print_battery_status(self):
-        print(self.attribute_status)
+        for (temperature, soc, charge_rate, expected_abnormality_number) in param_list_abnormality_number:
+            battery = Battery(temperature, soc, charge_rate)
+            self.assertEqual(battery.number_of_abnormalities(), expected_abnormality_number)
 
-    def display_abnormality_report(self):
-        self.update_attribute_status()
-        reporter = Reporter(self.attribute_status)
-        reporter.abnormality_report()
+    def test_battery_attribute_status(self):
+        param_list = [((-10, 50, 0.5), {'temperature': 'low'}), ((50, 50, 0.5), {'temperature': 'high'}), ((25, 10, 0.5), {'soc': 'low'}), 
+        ((25, 90, 0.5), {'soc': 'high'}), ((25, 50, -0.5), {'charge_rate': 'low'}), ((25, 50, 1.0), {'charge_rate': 'high'}), 
+        ((-10, 90, 0.7), {'temperature': 'low', 'soc': 'high'}), ((90, 14, -0.1), {'temperature': 'high', 'soc': 'low', 'charge_rate': 'low'}), 
+        ((65, 45, 0.98), {'temperature': 'high', 'charge_rate': 'high'})]
 
-    def get_attribute_satatus(self):
-        self.update_attribute_status()
-        return self.attribute_status
+        for ((temperature, soc, charge_rate), expected_attribute_status) in param_list:
+            battery = Battery(temperature, soc, charge_rate)
+            status = battery.get_attribute_satatus()
+            self.assertEqual(status, expected_attribute_status)
+
+if __name__ == '__main__':
+    unittest.main()
